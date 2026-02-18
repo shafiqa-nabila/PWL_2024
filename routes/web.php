84c114fd6c45abf5f097327a24ad86ac363dcb36
shafiqa-nabila <shafiqanabila9@gmail.com>
2026-02-18 16:48:30 +0700

    modifikasi photos

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
 
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
